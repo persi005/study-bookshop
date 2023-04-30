60f1dceee791c2cdc85971f534f57263d5de18c9
Vadim Serednev <vserednevdev@gmail.com>
2023-04-30 17:58:24 +0300

    configured rollup

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import css from 'rollup-plugin-import-css'
+
+export default {
+    input: 'src/app.js',
+    output: {
+        dir: 'dist',
+        format: 'iife',
+    },
+    plugins: [css(), nodeResolve()],
+}
