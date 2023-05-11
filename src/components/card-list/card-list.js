71a525a7f633a081bdbb081f78f52545bccdd55d
Vadim Serednev <vserednevdev@gmail.com>
2023-05-11 12:50:16 +0300

    card to card-list & style adjustments

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -1,4 +1,5 @@
 import { DivComponent } from '../../common/div-component.js'
+import { Card } from '../card/card.js'
 import './card-list.css'
 
 export class CardList extends DivComponent {
@@ -10,15 +11,24 @@ export class CardList extends DivComponent {
     }
 
     render() {
+        this.el.classList.add('card-list')
         if (this.parentState.loading) {
             this.el.innerHTML = `<p class="card_list__loader">Loading...</p>`
             return this.el
         }
 
-        this.el.classList.add('card_list')
         this.el.innerHTML = `
             <h1>Found: ${this.parentState.list.length}</h1>
         `
+        const cardGrid = document.createElement('div')
+        cardGrid.classList.add('card-list__inner')
+
+        for (const card of this.parentState.list) {
+            cardGrid.append(new Card(this.appState, card).render())
+        }
+
+        this.el.append(cardGrid)
+
         return this.el
     }
 }
